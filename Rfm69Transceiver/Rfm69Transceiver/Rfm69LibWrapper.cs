be382a26ef1816161466d4053e240deefc105fd9
Al Rodriguez <ProgrammerAl@outlook.com>
2019-05-05 22:09:54 -0400

    Made P/Invoke calls internal.

--- Rfm69Transceiver/Rfm69Transceiver/Rfm69LibWrapper.cs
+++ Rfm69Transceiver/Rfm69Transceiver/Rfm69LibWrapper.cs
@@ -5,27 +5,27 @@ namespace ProgrammerAl.HardwareSpecific.RF
     public static class Rfm69LibWrapper
     {
         [DllImport("./NativeLibs/RaspberryPiRfm69Wrapper.o", EntryPoint = "rfm69_initialize", SetLastError = true)]
-        public static extern int Initialize(Rf69FrequencyType frequencyBand, byte nodeId, byte networkId);
+        internal static extern int Initialize(Rf69FrequencyType frequencyBand, byte nodeId, byte networkId);
 
         [DllImport("./NativeLibs/RaspberryPiRfm69Wrapper.o", EntryPoint = "rfm69_setPowerLevel", SetLastError = true)]
-        public static extern void SetPowerLevel(TransmitPowerLevel powerLevel);
+        internal static extern void SetPowerLevel(TransmitPowerLevel powerLevel);
 
         [DllImport("./NativeLibs/RaspberryPiRfm69Wrapper.o", EntryPoint = "rfm69_send", SetLastError = true)]
-        public static extern void Send(byte targetNodeId, byte[] buffer, byte bufferSize, bool requestACK);
+        internal static extern void Send(byte targetNodeId, byte[] buffer, byte bufferSize, bool requestACK);
 
         [DllImport("./NativeLibs/RaspberryPiRfm69Wrapper.o", EntryPoint = "rfm69_receive", SetLastError = true)]
-        public static extern void Receive();
+        internal static extern void Receive();
 
         [DllImport("./NativeLibs/RaspberryPiRfm69Wrapper.o", EntryPoint = "rfm69_getDataLen", SetLastError = true)]
-        public static extern short GetMessageBytesLength();
+        internal static extern short GetMessageBytesLength();
 
         [DllImport("./NativeLibs/RaspberryPiRfm69Wrapper.o", EntryPoint = "rfm69_getRssi", SetLastError = true)]
-        public static extern int GetMessageRSSI();
+        internal static extern int GetMessageRSSI();
 
         [DllImport("./NativeLibs/RaspberryPiRfm69Wrapper.o", EntryPoint = "rfm69_getData", SetLastError = true)]
-        public static extern void GetMessageBytes(byte[] messageReceived);
+        internal static extern void GetMessageBytes(byte[] messageReceived);
 
         [DllImport("./NativeLibs/RaspberryPiRfm69Wrapper.o", EntryPoint = "rfm69_getSenderId", SetLastError = true)]
-        public static extern byte GetMessageSenderId();
+        internal static extern byte GetMessageSenderId();
     }
 }
